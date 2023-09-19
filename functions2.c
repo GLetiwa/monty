4b7bd6baefc5f414928bb7761ec945715f216281
GLetiwa <graceletiwa@gmail.com>
2023-09-19 21:55:52 +0300

    first

--- functions2.c
+++ functions2.c
@@ -1,5 +1,22 @@
 #include "monty.h"
 
+/*int is_stack_mode = 1;
+
+void opcode_stack(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	is_stack_mode = 1;
+}
+
+void opcode_queue(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+	is_stack_mode = 0;
+} */
+
+
 /**
  *
  *
